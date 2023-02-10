6dce966ba4ff239a401d8ddae94347077234656a
Michael Schmid <schmid.m9@googlemail.com>
2023-02-10 13:10:36 +0100

    edit json

--- js/data.js
+++
@@ -1,6 +0,0 @@
-{ 
-    "taskName": "shopping", 
-    "image": "img/groceries.jpg", 
-    "description": "Shop for weekly groceries at Billa and Hofer.", 
-    "importance": 0    
-  } 
---
+++ script.js
@@ -0,0 +1,3 @@
+const tasks = JSON.parse(toDo);
+
+console.log(tasks);
